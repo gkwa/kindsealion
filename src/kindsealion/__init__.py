381aadb45b8ac61b183e40ff9759ce0c782ceac1
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-17 16:03:02 -0400

    get stuff ftom t3

--- src/kindsealion/__init__.py
+++ src/kindsealion/__init__.py
@@ -1,44 +1,9 @@
-import dataclasses
-
-import ruamel.yaml
 
 from . import main2
 
 __project_name__ = "kindsealion"
 
 
-@dataclasses.dataclass
-class Builder:
-    name: str
-    script: str
-
-
 def main() -> int:
-    args = main2.parse_args()
-    outdir = args.outdir
-
-    yaml_parser = ruamel.yaml.YAML()
-    with open("manifest.yml", "r") as file:
-        data = yaml_parser.load(file)
-
-    manifests = []
-    for item in data:
-        manifests.append(
-            Builder(
-                name=item["name"],
-                script=item["script"],
-            )
-        )
-
-    template = main2.get_template("script.sh.j2")
-
-    outdir.mkdir(parents=True, exist_ok=True)
-    for manifest in manifests:
-        script_path = outdir / f"{manifest.name}.sh"
-        rendered_script = template.render(script=manifest.script)
-        with script_path.open("w") as script_file:
-            script_file.write(rendered_script)
-
-
-if __name__ == "__main__":
-    main()
+    main2.main()
+    return 0
--- src/kindsealion/main2.py
+++ src/kindsealion/main2.py
@@ -1,7 +1,9 @@
 import argparse
+import dataclasses
 import pathlib
 
 import jinja2
+import ruamel.yaml
 
 
 def get_template(template_name):
@@ -16,6 +18,12 @@ def render_template(template_name, data=None):
     return template.render(data=data)
 
 
+@dataclasses.dataclass
+class Builder:
+    name: str
+    script: ruamel.yaml.scalarstring.PreservedScalarString
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -25,3 +33,31 @@ def parse_args():
         default="kindsealion",
     )
     return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    outdir = args.outdir
+
+    yaml_parser = ruamel.yaml.YAML()
+    with open("manifest.yml", "r") as file:
+        data = yaml_parser.load(file)
+
+    manifests = []
+    for item in data:
+        manifests.append(
+            Builder(
+                name=item["name"],
+                script=ruamel.yaml.scalarstring.PreservedScalarString(item["script"]),
+            )
+        )
+
+    outdir.mkdir(parents=True, exist_ok=True)
+    for manifest in manifests:
+        script_path = outdir / f"{manifest.name}.sh"
+        with script_path.open("w") as script_file:
+            script_file.write(manifest.script)
+
+
+if __name__ == "__main__":
+    main()
