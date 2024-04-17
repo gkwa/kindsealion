c836781b7f89cff0311d5e56c79ade55015bcbbf
Taylor Monacelli <taylormonacelli@gmail.com>
2024-04-17 14:32:17 -0400

    jnja

--- src/kindsealion/main2.py
+++ src/kindsealion/main2.py
@@ -1,3 +1,4 @@
+import argparse
 import pathlib
 
 import jinja2
@@ -13,3 +14,14 @@ def get_template(template_name):
 def render_template(template_name, data=None):
     template = get_template(template_name)
     return template.render(data=data)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--outdir",
+        type=pathlib.Path,
+        help="Output directory",
+        default="kindsealion",
+    )
+    return parser.parse_args()
