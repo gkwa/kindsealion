efffbcecda68b629175ef71976ae80483f4924fe
Taylor Monacelli <taylormonacelli@gmail.com>
2024-05-02 17:10:58 -0700

    prepare peppy_pelican for testing ansible
    t
    
    t

--- rendered/011_peppy_pelican.sh
+++ rendered/011_peppy_pelican.sh
@@ -16,3 +16,8 @@ brew "ansible"
 EOF
 chmod -R a+rwx $d
 sudo --login --user linuxbrew bash -c "cd $d && brew bundle"
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export DEBIAN_FRONTEND=noninteractive
+sudo dpkg-reconfigure locales
+sudo --login --user linuxbrew brew install taylormonacelli/homebrew-tools/itmetrics
